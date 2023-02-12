5389af062583a5d04f51c52fccf2ceb316e694ca
leo <manjunathprakash549@gmail.com>
2023-02-12 14:21:30 +0530

    1676191834

---
+++ jstest.js
@@ -0,0 +1,60 @@
+var shortcut = require('steam-shortcut-editor');
+var fs = require('fs');
+
+var filePath = `/home/deck/.steam/steam/userdata/${process.argv[2]}/config/shortcuts.vdf`;
+
+shortcut.parseFile(filePath,
+    { autoConvertArrays: true, autoConvertBooleans: true, dateProperties: ['LastPlayTime'] },
+    function (err, obj, inputBuffer) {
+
+        if (err) {
+            console.log('failed to read ' + filePath);
+            return;
+        }
+
+        var gameListPC = []
+        var myobj
+        for (myobj of obj['shortcuts']) {
+            mydate = new Date(myobj['LastPlayTime'])
+            tempObject = {
+                gameName: myobj['AppName'],
+                lastPlayed: mydate.toDateString()
+            }
+
+            gameListPC.push(tempObject)
+        }
+
+        var gameListRepo = []
+        fs.readFileSync('./README.md').toString().split('\n').forEach(function (line) {
+            let split = line.trim().split(' | ')
+            if (split[0] != undefined) {
+                tempObject = {
+                    gameName: split[0],
+                    lastPlayed: split[1]
+                }
+                gameListRepo.push(tempObject)
+            }
+        });
+
+
+        var gameNameListPC = [];
+        for (item of gameListPC) {
+            gameNameListPC.push(item['gameName'])
+        }
+
+        // var gameNameListRepo =[];
+        // for (item of gameListRepo){
+        //     gameNameListRepo.push(item['gameName'])
+        // }
+
+        var logger2 = fs.createWriteStream('README.md')
+        logger2.write("# Games on deck")
+        for (listitem of gameListPC) {
+            logger2.write(`\n${listitem.gameName} | ${listitem.lastPlayed}  `)
+        }
+        for (listitem of gameListRepo) {
+            if (gameNameListPC.includes(listitem.gameName) || listitem.gameName == "# Games on deck")
+                continue
+            logger2.write(`\n~~${listitem.gameName}~~ | ${listitem.lastPlayed}  `)
+        }
+    });
